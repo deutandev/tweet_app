ef0cd7f32b1a0d7d4116b710a6d4eddea7c9ba18
deutandev <deutandev@pm.me>
2022-04-07 14:08:14 +0700

    Add functionality to save the new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,11 @@ class PostsController < ApplicationController
   def new
     
   end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    
+    redirect_to("/posts/index")
+  end
 end
