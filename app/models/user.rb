255763735c5015a65e29d36dc7729cf052d1dae6
deutandev <deutandev@pm.me>
2022-04-10 22:53:58 +0700

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    # Validate that the name column is not empty
+    validates :name, {presence: true}
+    
+    # Validate that the email column is unique and not empty
+    validates :email, {presence: true, uniqueness: true}
 end
