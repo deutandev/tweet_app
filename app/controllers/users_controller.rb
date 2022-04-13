f24b4481786dcfc4ee1eb631ede6cbf5c7280145
deutandev <deutandev@pm.me>
2022-04-13 09:16:28 +0700

    Add: User authentication to edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
   before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+
 
   def index
     @users = User.all
@@ -83,5 +85,11 @@ class UsersController < ApplicationController
     redirect_to("/login")
   end
 
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "Unauthorized access"
+      redirect_to("/posts/index")
+    end
+  end
   
 end
