116b740ff440e9365c0fb886e115f13128b32b6f
deutandev <deutandev@pm.me>
2022-04-13 18:47:46 +0700

    Add: password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
-    # Validate that the name column is not empty
+    has_secure_password
+
     validates :name, {presence: true}
-    
-    # Validate that the email column is unique and not empty
     validates :email, {presence: true, uniqueness: true}
 
-    validates :password, {presence: true}
 
     def posts
         return Post.where(user_id: self.id)
     end
-    
+
 end
+  
